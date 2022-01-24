4ee9ed6ffb3f0d95e6aef8ca9f7b8a54406ac77b
DeniseBarbosa <deniseluvoir@gmail.com>
2022-01-24 13:37:53 -0300

    Fetch 1

---
+++ script.js
@@ -0,0 +1,13 @@
+function carregarPostagem(){
+    document.getElementById("posts").innerHTML = 'Carregando...'
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(function(resultado){
+        return resultado.json();
+    })
+    .then(function(json){
+        document.getElementById("posts").innerHTML = json.length+' posts';
+    })
+    .catch(function(error){
+        console.log("Erro!")
+    });
+}
